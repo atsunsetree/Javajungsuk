0fd583df74864ceafb7e1a16a4eb3a5f65d249a1
atsunsetree <atsunsetree529@gmail.com>
2023-07-25 20:05:39 +0900

    ch12 연습문제 풀이

---
+++ ch12/ex04.java
@@ -0,0 +1,21 @@
+package ch12;
+
+import ch07.Product;
+
+import java.util.ArrayList;
+
+public class ex04 {
+    public static void main(String[] args) {
+    }
+/*
+    public static <T extends Product> ArrayList<T> merge(
+            ArrayList<T> list1, ArrayList<T> list2) {
+        ArrayList<T> newList = new ArrayList<>(list1);
+
+        newList.addAll(list2);
+
+        return newList;
+    }
+
+ */
+}
